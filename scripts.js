843f7dd489f5306ed90eea3482feb72fa2b1ea2a
imhiju7 <imhiju7@gmail.com>
2024-09-25 13:53:30 +0700

    graph contribute

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Add squares
+const squares = document.querySelector('.squares');
+for (var i = 1; i < 365; i++) {
+  const level = Math.floor(Math.random() * 4);  
+  squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
+}
